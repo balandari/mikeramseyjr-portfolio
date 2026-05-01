fb9a6a1183bf1bade00524f559cca7113b0265fa
Mike Ramsey Jr <55157823+balandari@users.noreply.github.com>
2026-05-01 18:10:21 -0500

    Add rewrites for /app tracker routes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/app',
+        destination: '/app.html',
+      },
+      {
+        source: '/app/learning',
+        destination: '/app/learning.html',
+      },
+    ];
+  },
+};
 
 export default nextConfig;
